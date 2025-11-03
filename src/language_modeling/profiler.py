a985317c1e88bcccd56ed97898c5819287efde36
Rashid <rashidstanikzai.net@gmail.com>
2025-11-03 17:37:18 +0500

    Use direct access for cuda_time_total in profiler
    Replaces getattr with direct attribute access for model_inference_event.cuda_time_total. This change assumes the attribute is always present and simplifies the code.

--- src/language_modeling/profiler.py
+++ src/language_modeling/profiler.py
@@ -98,8 +98,8 @@ if __name__ == "__main__":
             break
 
     total_cpu_time = model_inference_event.cpu_time_total/1000**2 / num_trails
-    # total_cuda_time = model_inference_event.cuda_time_total/1000**2 / num_trails
-    total_cuda_time = getattr(model_inference_event, 'cuda_time_total', 0)/1000**2 / num_trails
+    total_cuda_time = model_inference_event.cuda_time_total/1000**2 / num_trails
+    # total_cuda_time = getattr(model_inference_event, 'cuda_time_total', 0)/1000**2 / num_trails
     total_gflops = sum([event.flops for event in events]) / 1e9 / num_trails
     
     result_dict =  {
